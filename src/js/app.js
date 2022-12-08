87cdccd3d6367797237b954d151dd2d90685ac87
Igor Timonin <mut4nt@yandex.ru>
2022-12-08 15:33:17 +0300

    parallax layers adjusted

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
+//получение значения вертикального скролла
 window.addEventListener('scroll', e => {
   document.documentElement.style.setProperty(
     '--scrollTop',
     `${this.scrollY}px`
   );
-  // document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
 });
 
+//подключение плагинов gsap
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 ScrollSmoother.create({
   wrapper: '.wrapper',
   content: '.content'
 });
-
