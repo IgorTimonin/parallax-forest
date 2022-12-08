501315e4ad28a2959db5b830b4c145859a34b0c4
Igor Timonin <mut4nt@yandex.ru>
2022-12-08 18:34:48 +0300

    add audio

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 //получение значения вертикального скролла
-window.addEventListener('scroll', e => {
+window.addEventListener('scroll', (e) => {
   document.documentElement.style.setProperty(
     '--scrollTop',
     `${this.scrollY}px`
@@ -7,8 +7,29 @@ window.addEventListener('scroll', e => {
 });
 
 //подключение плагинов gsap
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 ScrollSmoother.create({
   wrapper: '.wrapper',
-  content: '.content'
+  content: '.content',
 });
+
+//управление плеером
+// const audio = new Audio(
+//   './src/audio/alexander-nakarada-fairy-of-the-forest.mp3'
+// );
+
+// audio.addEventListener('canplaythrough', () => {
+//   audio.play();
+// });
+
+// const audio = document.getElementById('audio');
+const playBtn = document.getElementById('playBtn');
+
+function playPause() {
+  if (playBtn.classList.contains('btn-pause')) {
+    audio.pause();
+  } else audio.play();
+  playBtn.classList.toggle('btn-pause');
+}
+
+playBtn.addEventListener('click', playPause);
