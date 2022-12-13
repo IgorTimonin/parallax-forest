2b19ad0b8adcb6b1e7e96a60265bd6ea1f099a49
Igor Timonin <mut4nt@yandex.ru>
2022-12-13 19:40:38 +0300

    add observer function

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,7 @@ window.addEventListener('scroll', (e) => {
     '--scrollTop',
     `${this.scrollY}px`
   );
-  document.documentElement.style.setProperty(
-    '--scrollData',
-    this.scrollY
-  );
+  document.documentElement.style.setProperty('--scrollData', this.scrollY);
 });
 
 //подключение плагинов gsap
@@ -28,3 +25,42 @@ function playPause() {
 }
 
 playBtn.addEventListener('click', playPause);
+
+//Появление сообщений
+window.onload = () => {
+  const options = {
+    root: null,
+    rootMatgin: '0px',
+    threshold: 0.5,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const lazyText = entry.target;
+        console.log(lazyText);
+        lazyText.classList.add('floor__content_show');
+        observer.unobserve(lazyText);
+      }
+    });
+  }, options);
+
+  const arr = document.querySelectorAll('floor__content');
+  arr.forEach((i) => {
+    observer.observe(i);
+  });
+};
+
+// function onEntry(entry) {
+//   entry.forEach((change) => {
+//     if (change.isIntersecting) {
+//       change.target.classList.add('floor__content_show');
+//     }
+//   });
+// }
+// let options = { threshold: [0.5] };
+// let observer = new IntersectionObserver(onEntry, options);
+// let elements = document.querySelectorAll('.floor__content_animation');
+// for (let elm of elements) {
+//   observer.observe(elm);
+// };
